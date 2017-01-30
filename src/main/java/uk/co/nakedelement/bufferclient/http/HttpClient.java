541272201f8f66ab433ab56517189372e12ff0da
Paul Grenyer <paul.grenyer@gmail.com>
2017-01-30 11:13:41 +0000

    Added post.

--- src/main/java/uk/co/nakedelement/bufferclient/http/HttpClient.java
+++ src/main/java/uk/co/nakedelement/bufferclient/http/HttpClient.java
@@ -2,6 +2,7 @@ package uk.co.nakedelement.bufferclient.http;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -12,7 +13,9 @@ import java.util.Map;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -57,6 +60,25 @@ public class HttpClient
 		}
 	}
 	
+	public Response post(String url, Map<String, String> params, Map<String, String> bodyParams)
+	{
+		try 
+		{
+			final String finalUrl = buildUrl(url);						
+			final HttpPost request = new HttpPost(finalUrl);
+			buildRequest(request, params, bodyParams);            
+            log.debug(request.getURI());
+            
+			final HttpResponse response = execute(request);
+			return new Response(response.getStatusLine().getStatusCode(), toString(response.getEntity().getContent()));
+		}
+		catch (IOException | URISyntaxException e)
+		{
+			log.error(e);
+			throw new HttpClientException(e);
+		}
+	}
+	
 	private static HttpRequestBase buildRequest(HttpRequestBase request, Map<String, String> params) throws URISyntaxException
 	{
 		final List<NameValuePair> nameValuePairs = new ArrayList<>();
@@ -66,6 +88,18 @@ public class HttpClient
 		final URI uri = new URIBuilder(request.getURI()).addParameters(nameValuePairs).build();
         request.setURI(uri);
         return request;
+	}
+	
+	private static HttpRequestBase buildRequest(HttpPost request, Map<String, String> params, Map<String, String> bodyParams) throws URISyntaxException, UnsupportedEncodingException
+	{
+		request = (HttpPost) buildRequest(request, params);
+		
+		final List<NameValuePair> basicBodyparams = new ArrayList<NameValuePair>();
+		for(final String key : bodyParams.keySet())
+			basicBodyparams.add(new BasicNameValuePair(key, bodyParams.get(key)));			
+		request.setEntity(new UrlEncodedFormEntity(basicBodyparams, ENCODING));
+		
+		return request;
 	}
 	
 	private static HttpResponse execute(HttpRequestBase request) throws IOException

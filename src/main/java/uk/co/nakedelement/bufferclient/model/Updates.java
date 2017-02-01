b70ea492276342460077377a4d086627e9004551
Paul Grenyer <paul.grenyer@gmail.com>
2017-02-01 15:47:34 +0000

    Better toString().

--- src/main/java/uk/co/nakedelement/bufferclient/model/Updates.java
+++ src/main/java/uk/co/nakedelement/bufferclient/model/Updates.java
@@ -1,18 +1,43 @@
 package uk.co.nakedelement.bufferclient.model;
 
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @XmlRootElement
 public class Updates
 {
-	  @XmlElement(name="total")
-	  private int total;
+	@JsonProperty("success")
+	private boolean success;
+	
+	@JsonProperty("message")
+	private String message;
+	
+	@JsonProperty("total")
+	private int total;
 	  
-	  @XmlElement(name="total")
-	  private Update[] updates;
+	@JsonProperty("updatesl")
+	private Update[] updates;
+	  
+	  
+
+		public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
 		public int getTotal() {
 			return total;
@@ -33,16 +58,28 @@ public class Updates
 		@Override
 		public String toString()
 		{
-			final StringBuilder builder = new StringBuilder("total=");
-			builder.append(getTotal());
-			builder.append(", [");
-			for(final Update update : getUpdates())
+			final StringBuilder builder = new StringBuilder();
+			
+			if (!isSuccess())
 			{
-				builder.append("{");
-				builder.append(update);
-				builder.append("}, ");
+				builder.append("message=");
+				builder.append(getMessage());
 			}
-			builder.append("]");
+			else
+			{
+				
+				builder.append("total=");
+				builder.append(getTotal());
+				builder.append(", [");
+				for(final Update update : getUpdates())
+				{
+					builder.append("{");
+					builder.append(update);
+					builder.append("}, ");
+				}
+				builder.append("]");
+			}
 			return builder.toString();
+		
 		}	
 }
